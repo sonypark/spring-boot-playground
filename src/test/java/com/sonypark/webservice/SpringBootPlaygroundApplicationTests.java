8436c808550b7bbcb6715663f4a0d487d807ddf0
Sony <sonypark0204@gmail.com>
2020-05-15 17:38:41 +0900

    feat: initial commit

---
+++ src/test/java/com/sonypark/webservice/SpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonypark.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
